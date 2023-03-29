91a2bdd7b814536d770b54b158399ef59adb10d6
Pawan Jaypal Kamble <121616745+pawank18797@users.noreply.github.com>
2023-03-29 13:29:24 +0530

    save changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region "ap-south-1"
-access_key = "AKIAQHLHWRPHKU4M52TX"
-secret_key = "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"
+access_key = "AKIAQHLHWRPHCVZLUS5X"
+secret_key = "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"
 }
